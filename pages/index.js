0a4186ab6f9a544f4157e1f67749a4c027fd8c3a
malweis <58945171+malweis@users.noreply.github.com>
2023-05-14 10:51:15 -0400

    feat: adds post fetching into the page
    Post now load from graphcsm from the requests at
    services index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 
 import { PostCard, Categories, PostWidget } from '../components';
 import Head from 'next/head'
+import {getPosts} from '../services'
 
 
-const posts = [
-  {
-    title: 'React testing', excerpt: 'Learn React Testing'
-  },
-  {
-    title: 'React with Tailwind', excerpt: 'Learn React Tailwind'
-  },
-
-];
-
-export default function Home() {
+export default function Home({posts}) {
   return (
 
 
@@ -26,7 +17,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
       <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post) => <PostCard post={post} key={post.title} />)}
+      {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -37,4 +28,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts() || []);
+  
+  return {
+    props: {posts}
+  }
 }
